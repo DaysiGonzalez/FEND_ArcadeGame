5e9f8be112b0e0c4fcf52e4cb46bb772bde69b09
DaysiGonzalez <dsgv11@hotmail.com>
2019-03-13 22:23:09 -0600

    Player is added at start position

--- js/app.js
+++ js/app.js
@@ -19,6 +19,7 @@ Enemy.prototype.update = function(dt) {
     // all computers.
     this.x += this.speed * dt;
     if (this.x > 505){
+      //If x position is outside the board, then reset position to the start and assign a new random speed
       this.x = -100;
       this.speed = (Math.random()* (300 - 60) + 60);
     }
@@ -32,7 +33,23 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(){
+  this.sprite = 'images/char-boy.png';
+  this.x = 200;
+  this.y = 400;
+}
 
+Player.prototype.update = function(dt){
+
+}
+
+Player.prototype.render = function(){
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(direction){
+
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -41,8 +58,7 @@ let enemy1 = new Enemy(-100,60,(Math.random()* (300 - 60) + 60));
 let enemy2 = new Enemy(-100,140,(Math.random()* (300 - 60) + 60));
 let enemy3 = new Enemy(-100,230,(Math.random()* (300 - 60) + 60));
 let allEnemies = [enemy1, enemy2, enemy3];
-
-
+let player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -54,5 +70,5 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
 
-    //player.handleInput(allowedKeys[e.keyCode]);
+    player.handleInput(allowedKeys[e.keyCode]);
 });
