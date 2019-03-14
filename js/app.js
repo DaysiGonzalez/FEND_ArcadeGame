d7bc7cb59814a34f01ec774c15bf1536d9f84de6
DaysiGonzalez <dsgv11@hotmail.com>
2019-03-13 22:02:01 -0600

    Enemies update, start over

--- js/app.js
+++ js/app.js
@@ -17,11 +17,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-
+console.log("updating");
     this.x += this.speed * dt;
     if (this.x > 505){
+      console.log("out");
       this.x = -100;
-      this.speed = Math.floor(Math.random());
+      this.speed = (Math.random()*60);
     }
 };
 
